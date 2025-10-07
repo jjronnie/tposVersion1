7c8d208f25f6febcdff494124b72a88a858f30a9
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-10-08 00:33:17 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             ProtectAgainstSpam::class,
-            //  EnsureBusinessReady::class,
+             EnsureBusinessReady::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
