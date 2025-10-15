9f273cd6c13fa578bad1e0efbacc61fa6ff20701
Jjuuko Ronald <ronaldjjuuko7@gmail.com>
2025-10-15 10:14:07 +0300

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -95,7 +95,7 @@
 <footer class="bg-white border-t border-gray-200 py-4">
     <div
         class="max-w-7xl mx-auto px-4 md:px-6 flex flex-col sm:flex-row justify-between items-center text-sm text-gray-500 space-y-2 sm:space-y-0">
-        <p>&copy; {{ date('Y') }} <a href="http://techtowerinc.com" target="_blank" rel="noopener noreferrer"></a> TechTower Inc. | All rights reserved.</p>
+        <p>&copy; {{ date('Y') }} <a href="http://techtowerinc.com" target="_blank" rel="noopener noreferrer"> TechTower Inc. </a>| All rights reserved.</p>
 
         <p>Version 1.0.0</p>
     </div>
